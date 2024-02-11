08c496f84267da00397090ca0d8330958dbbf668
Raju Chettri <dada@Rajus-MacBook-Pro.local>
2024-02-11 15:17:59 -0500

    hero section fixes done

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Hero />
       <Camp/>
       <Guide/>
       <Features/>
       <GetApp/>
-    </>
+    </div>
   );
 
 }
